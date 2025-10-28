bb07215ba3684aad02f5915d51459f35f8ffaa90
Nikola Sibalic <nikola@quantum-machines.co>
2025-10-28 17:09:24 +0100

    fixed import, bumped version

--- iqcc_qualibrate2cloud/__init__.py
+++ iqcc_qualibrate2cloud/__init__.py
@@ -1,4 +1,4 @@
-from qualibrate_cloud_handler import QualibrateCloudHandler
+from iqcc_qualibrate2cloud.qualibrate_cloud_handler import QualibrateCloudHandler
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["QualibrateCloudHandler"]
